40a50df03ae10cd9e1a3b4855b9900f6c99cf489
alonsocamilla1 <alocamilla@gmail.com>
2023-03-20 09:20:56 -0300

    Adiçao da Tranferencia

--- src/main/java/br/gama/itau/projetofinal2/controller/ContaController.java
+++ src/main/java/br/gama/itau/projetofinal2/controller/ContaController.java
@@ -1,24 +1,21 @@
 package br.gama.itau.projetofinal2.controller;
 
 import java.util.List;
-//import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-//import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-//import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-//import br.gama.itau.projetofinal2.exception.NotFoundException;
 import br.gama.itau.projetofinal2.model.Conta;
-//import br.gama.itau.projetofinal2.repositorio.ContaRepo;
 import br.gama.itau.projetofinal2.service.ContaService;
+import br.gama.itau.projetofinal2.service.TransferenciaService;
 
 
 
@@ -29,11 +26,9 @@ public class ContaController {
     @Autowired
     private ContaService contaService;
     
- //   @Autowired
- //   private ContaRepo contaRepo;
-
+    @Autowired
+    TransferenciaService transferenciaService;
     
-
     // - /contas (POST) - para cadastrar uma nova conta, chamando o serviço
     // adicionarConta, podendo retornar 201 ou 400
     @PostMapping // OK
@@ -59,17 +54,13 @@ public class ContaController {
         List<Conta> conta = contaService.recuperarContasPeloCliente(id);
         return ResponseEntity.ok().body(conta);
     }
-
-  /*  @PutMapping
-    public ResponseEntity<Object> alterarDados(@PathVariable @RequestBody Conta conta, int numeroConta) {
-        Conta contaSalva = contaRepo.findByConta(conta)
-            .orElseThrow(() -> new NotFoundException("Conta naão encontrada"));
-        Conta contaAtualizada = contaService.alterarDados(conta, saldo);
-        if(contaAtualizada == null) {
-            return ResponseEntity.badRequest().build();
-        }
-        return ResponseEntity.ok(contaAtualizada);
-    } */
+    
+    @PostMapping("/transferencia")
+    public boolean transferencia(@RequestParam("contaOrigem") int contaOrigem,
+                                 @RequestParam("contaDestino") int contaDestino,
+                                 @RequestParam("valor") double valor) {
+        return transferenciaService.TransferirValores(contaOrigem, contaDestino, valor);
+    }
 }    
     
     
---
+++ src/main/java/br/gama/itau/projetofinal2/service/TransferenciaService.java
@@ -0,0 +1,32 @@
+package br.gama.itau.projetofinal2.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import br.gama.itau.projetofinal2.model.Conta;
+import br.gama.itau.projetofinal2.repositorio.ContaRepo;
+
+@Service
+public class TransferenciaService {
+
+    @Autowired
+    private ContaRepo contaRepository;
+    public boolean TransferirValores(int contaOrigem, int contaDestino, double valor) {
+        Conta contaOrigemObj = contaRepository.findById(contaOrigem)
+                .orElseThrow(() -> new IllegalArgumentException("Conta de origem inválida"));
+        Conta contaDestinoObj = contaRepository.findById(contaDestino)
+                .orElseThrow(() -> new IllegalArgumentException("Conta de destino inválida"));
+
+        if (contaOrigemObj.getSaldo() >= valor) {
+            contaOrigemObj.setSaldo(contaOrigemObj.getSaldo() - valor);
+            contaDestinoObj.setSaldo(contaDestinoObj.getSaldo() + valor);
+            contaRepository.save(contaOrigemObj);
+            contaRepository.save(contaDestinoObj);
+            return true;
+        } else {
+            return false;
+        }
+
+    }
+
+}
